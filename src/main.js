929116ee811360e5e72a783b1a3c1dbcb859e856
Ferdinand Thiessen <opensource@fthiessen.de>
2023-08-02 19:36:18 +0200

    Produce rollup issue
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// run `npm run exec:dev` in the terminal to run the original file
+// run `npm run exec:build` in the terminal to run the built file
+
+import { foo } from 'dummy-exported/foo.js';
+
+console.log(`foo === ${foo}`);
